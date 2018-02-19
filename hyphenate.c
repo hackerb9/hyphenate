594040a48cb1c0ba386de5fef0f2d7ce873fb0f4
b9 <hackerb9@member.fsf.org>
2018-02-18 17:21:19 -0800

    reindent to make all lines consistent

--- hyphenate.c
+++ hyphenate.c
@@ -10,96 +10,96 @@
 
 char *progname;			/* short program name, set by main */
 void help() {
-    printf("Usage:\n"); 
-    printf("%s [-o | -d | -n] <word ...>\n", progname);
-    printf("\t-o = use old algorithm (without non-standard hyphenation)\n");
-    printf("\t-d = hyphenation with listing of the possible hyphenations\n");
-    printf("\t-n = print hyphenation vector\n");
+  printf("Usage:\n"); 
+  printf("%s [-o | -d | -n] <word ...>\n", progname);
+  printf("\t-o = use old algorithm (without non-standard hyphenation)\n");
+  printf("\t-d = hyphenation with listing of the possible hyphenations\n");
+  printf("\t-n = print hyphenation vector\n");
 }
 
 /* get a pointer to the nth 8-bit or UTF-8 character of the word */
 char * hindex(char * word, int n, int utf8) {
-    int j = 0;
-    while (j < n) {
-        j++;
-        word++;
-        while (utf8 && ((((unsigned char) *word) >> 6) == 2)) word++;
-    }
-    return word;
+  int j = 0;
+  while (j < n) {
+    j++;
+    word++;
+    while (utf8 && ((((unsigned char) *word) >> 6) == 2)) word++;
+  }
+  return word;
 }
 
 /* list possible hyphenations with -dd option (example for the usage of the hyphenate2() function) */
 void single_hyphenations(char * word, char * hyphen, char ** rep, int * pos, int * cut, int utf8) {
-    int i, k, j = 0;
-    char r;
-    for (i = 0; (i + 1) < strlen(word); i++) {
-        if (utf8 && ((((unsigned char) word[i]) >> 6) == 2)) continue;
-        if ((hyphen[j] & 1)) {
-            if (rep && rep[j]) {
-              k = hindex(word, j - pos[j] + 1, utf8) - word;
-              r = word[k];
-              word[k] = 0;
-              printf(" - %s%s", word, rep[j]);
-              word[k] = r;
-              printf("%s\n", hindex(word + k, cut[j], utf8));
-            } else {
-              k = hindex(word, j + 1, utf8) - word;
-              r = word[k];
-              word[k] = 0;
-              printf(" - %s=", word);
-              word[k] = r;
-              printf("%s\n", word + k);
-            }
-        }
-        j++;
+  int i, k, j = 0;
+  char r;
+  for (i = 0; (i + 1) < strlen(word); i++) {
+    if (utf8 && ((((unsigned char) word[i]) >> 6) == 2)) continue;
+    if ((hyphen[j] & 1)) {
+      if (rep && rep[j]) {
+	k = hindex(word, j - pos[j] + 1, utf8) - word;
+	r = word[k];
+	word[k] = 0;
+	printf(" - %s%s", word, rep[j]);
+	word[k] = r;
+	printf("%s\n", hindex(word + k, cut[j], utf8));
+      } else {
+	k = hindex(word, j + 1, utf8) - word;
+	r = word[k];
+	word[k] = 0;
+	printf(" - %s=", word);
+	word[k] = r;
+	printf("%s\n", word + k);
+      }
     }
+    j++;
+  }
 }
 
 int 
 main(int argc, char** argv)
 {
 
-    HyphenDict *dict;
-    int wtc;
-    FILE* wtclst;
-    int k, n, i, j, c;
-    char buf[BUFSIZE + 1];
-    int  nHyphCount;
-    char *hyphens;
-    char *lcword;
-    char *hyphword;
-    char hword[BUFSIZE * 2];
-    int arg = 1;
-    int optd = 1;
-    int optn = 0;
-    int optdd = 0;
-    char ** rep;
-    int * pos;
-    int * cut;
+  HyphenDict *dict;
+  int wtc;
+  FILE* wtclst;
+  int k, n, i, j, c;
+  char buf[BUFSIZE + 1];
+  int  nHyphCount;
+  char *hyphens;
+  char *lcword;
+  char *hyphword;
+  char hword[BUFSIZE * 2];
+  int arg = 1;
+  int optd = 1;
+  int optn = 0;
+  int optdd = 0;
+  char ** rep;
+  int * pos;
+  int * cut;
 
-    /* what name to show for usage message */
-    progname=strdup(basename(argv[0]));
+  /* what name to show for usage message */
+  progname=strdup(basename(argv[0]));
 
-    /* default dictionary file */
-    char *dictfile="/usr/share/hyphen/hyph_en_US.dic";
+  /* default dictionary file */
+  char *dictfile="/usr/share/hyphen/hyph_en_US.dic";
 
   /* first parse the command line options */
   /* arg1 - hyphen dictionary file, arg2 - file of words to check */
 
   if (argv[arg]) {
-       if (strcmp(argv[arg], "-o") == 0) {
-            optd = 0;
-            arg++;
-       }
-       if (strcmp(argv[arg], "-n") == 0) {
-            optn = 1;
-            arg++;
-       }
-       if (argv[arg] && strcmp(argv[arg], "-d") == 0) {
-            optd = 1;
-            optdd = 1;
-            arg++;
-       }
+    if (strcmp(argv[arg], "-o") == 0) {
+      optd = 0;
+      arg++;
+    }
+    if (strcmp(argv[arg], "-n") == 0) {
+      optn = 1;
+      arg++;
+    }
+    if (argv[arg] && strcmp(argv[arg], "-d") == 0) {
+      optd = 1;
+      optdd = 1;
+      arg++;
+    }
   }
 
   if (!argv[arg]) {
@@ -109,10 +109,10 @@ main(int argc, char** argv)
 
   /* load the hyphenation dictionary */  
   if ((dict = hnj_hyphen_load(dictfile)) == NULL) {
-       fprintf(stderr, "Could not load dictionary file\n");
-       perror(dictfile);
-       fflush(stderr);
-       exit(1);
+    fprintf(stderr, "Could not load dictionary file\n");
+    perror(dictfile);
+    fflush(stderr);
+    exit(1);
   }
 
   
@@ -120,85 +120,85 @@ main(int argc, char** argv)
   while (arg<argc) {
     char *word = argv[arg++];
     
-       k = strlen(word);
-       if (k && word[k - 1] == '\n') word[k - 1] = '\0';
-       if (k >=2 && word[k - 2] == '\r') word[k-- - 2] = '\0';
+    k = strlen(word);
+    if (k && word[k - 1] == '\n') word[k - 1] = '\0';
+    if (k >=2 && word[k - 2] == '\r') word[k-- - 2] = '\0';
 
-       /* set aside some buffers to hold lower cased */
-       /* and hyphen information */
-       lcword = (char *) malloc(k+1);
-       hyphens = (char *)malloc(k+5);
-       /* basic ascii lower-case, not suitable for real-world usage*/
-       for (i = 0; i < k; ++i) {
-         lcword[i] = word[i];
-         if ( (lcword[i] >= 'A') && (lcword[i] <= 'Z') )
-           lcword[i] += 32;
-       }
-
-       /* first remove any trailing periods */
-       n = k-1;
-       while((n >=0) && (lcword[n] == '.')) n--;
-       n++;
-
-       /* now actually try to hyphenate the word */
-       
-       rep = NULL;
-       pos = NULL;
-       cut = NULL;
-       hword[0] = '\0';
-
-       if ((!optd && hnj_hyphen_hyphenate(dict, lcword, n-1, hyphens)) ||
-	    (optd && hnj_hyphen_hyphenate2(dict, lcword, n-1, hyphens, hword, &rep, &pos, &cut))) {
-             free(hyphens);
-             free(lcword);
-             fprintf(stderr, "hyphenation error\n");
-             exit(1);
-       }
-
-       if (optn) fprintf(stderr, "%s\n", hyphens); 
-
-       if (!optd) {
-         /* now backfill hyphens[] for any removed periods */
-         for (c = n; c < k; c++) hyphens[c] = '0';
-         hyphens[k] = '\0';
-
-         /* now create a new char string showing hyphenation positions */
-         /* count the hyphens and allocate space for the new hypehanted string */
-         nHyphCount = 0;
-         for (i = 0; i < n; i++)
-           if (hyphens[i]&1)
-             nHyphCount++;
-         hyphword = (char *) malloc(k+1+nHyphCount);
-         j = 0;
-         for (i = 0; i < n; i++) {
-	   hyphword[j++] = word[i];
-           if (hyphens[i]&1) {
-	      hyphword[j++] = '-';
-	   }
-         }
-         hyphword[j] = '\0';
-         fprintf(stdout,"%s\n",hyphword);
-         fflush(stdout);
-         free(hyphword);
-      } else {
-         fprintf(stdout,"%s\n", hword);
-
-
-         if (optdd) single_hyphenations(lcword, hyphens, rep, pos, cut, dict->utf8);
-         if (rep) {
-            for (i = 0; i < n - 1; i++) {
-                if (rep[i]) free(rep[i]);
-            }
-            free(rep);
-            free(pos);
-            free(cut);
-         }
-      }
-      free(hyphens);
-      free(lcword);
+    /* set aside some buffers to hold lower cased */
+    /* and hyphen information */
+    lcword = (char *) malloc(k+1);
+    hyphens = (char *)malloc(k+5);
+    /* basic ascii lower-case, not suitable for real-world usage*/
+    for (i = 0; i < k; ++i) {
+      lcword[i] = word[i];
+      if ( (lcword[i] >= 'A') && (lcword[i] <= 'Z') )
+	lcword[i] += 32;
     }
 
-    fclose(wtclst);
-    hnj_hyphen_free(dict);
-    return 0;
+    /* first remove any trailing periods */
+    n = k-1;
+    while((n >=0) && (lcword[n] == '.')) n--;
+    n++;
+
+    /* now actually try to hyphenate the word */
+       
+    rep = NULL;
+    pos = NULL;
+    cut = NULL;
+    hword[0] = '\0';
+
+    if ((!optd && hnj_hyphen_hyphenate(dict, lcword, n-1, hyphens)) ||
+	(optd && hnj_hyphen_hyphenate2(dict, lcword, n-1, hyphens, hword, &rep, &pos, &cut))) {
+      free(hyphens);
+      free(lcword);
+      fprintf(stderr, "hyphenation error\n");
+      exit(1);
+    }
+
+    if (optn) fprintf(stderr, "%s\n", hyphens); 
+
+    if (!optd) {
+      /* now backfill hyphens[] for any removed periods */
+      for (c = n; c < k; c++) hyphens[c] = '0';
+      hyphens[k] = '\0';
+
+      /* now create a new char string showing hyphenation positions */
+      /* count the hyphens and allocate space for the new hypehanted string */
+      nHyphCount = 0;
+      for (i = 0; i < n; i++)
+	if (hyphens[i]&1)
+	  nHyphCount++;
+      hyphword = (char *) malloc(k+1+nHyphCount);
+      j = 0;
+      for (i = 0; i < n; i++) {
+	hyphword[j++] = word[i];
+	if (hyphens[i]&1) {
+	  hyphword[j++] = '-';
+	}
+      }
+      hyphword[j] = '\0';
+      fprintf(stdout,"%s\n",hyphword);
+      fflush(stdout);
+      free(hyphword);
+    } else {
+      fprintf(stdout,"%s\n", hword);
+
+
+      if (optdd) single_hyphenations(lcword, hyphens, rep, pos, cut, dict->utf8);
+      if (rep) {
+	for (i = 0; i < n - 1; i++) {
+	  if (rep[i]) free(rep[i]);
+	}
+	free(rep);
+	free(pos);
+	free(cut);
+      }
+    }
+    free(hyphens);
+    free(lcword);
+  }
+
+  fclose(wtclst);
+  hnj_hyphen_free(dict);
+  return 0;
 }
